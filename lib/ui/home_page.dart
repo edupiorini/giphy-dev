d765331022d43ddfb1a9d8e6b59d89a47ca5a271
edupiorini <eduardopiorini@hotmail.com>
2020-09-24 07:05:57 -0300

    bug fix on _offset when performing new search

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -54,6 +54,7 @@ class _HomePageState extends State<HomePage> {
               onSubmitted: (text) {
                 setState(() {
                   _search = text;
+                  _offset = 0;
                 });
               },
             ),
